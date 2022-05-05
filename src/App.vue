5d79e85a736c6d7e656ca0b80311dad93bd5546d
0gravity000 <0gravit000@gmail.com>
2022-05-05 22:21:05 +0900

    bootstrapを追加。NavbarMainを追加

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <navbar-main />
   <router-view/>
 </template>
+
+<script>
+// @ is an alias to /src
+import NavbarMain from '@/components/NavbarMain.vue'
+
+export default {
+  components: {
+    NavbarMain,
+  },
+}
+
+</script>
 
 <style>
 #app {
